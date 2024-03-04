7fab2650462a1e259fbe76565297dd0699fec3b8
Artag/Chris <scristxyz@gmail.com>
2024-03-04 14:52:49 -0500

    terminado la peticion http

--- lib/domain/datasources/movies_datasource.dart
+++ lib/domain/datasources/movies_datasource.dart
@@ -1,30 +1,17 @@
 
-import 'package:dio/dio.dart';
 
-import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 
 abstract class MoviesDatasource{
 
-final dio=Dio(BaseOptions(
-  baseUrl:"https://api.themoviedb.org/3",
-  queryParameters: {
-    "api_key":Environment.theMovieDbKey,
-    "language":"es-MX"
-  }
-));
-
-
-
-
 
    Future<List<Movie>> getNowPlaying({int page =1})async{
 
-final resonse = await dio.get("/movie/now_playing");
-final List<Movie> movies=[];
 
 
-    return movies;
+
+    
+    return [];
    }
 
 }
---
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -0,0 +1,29 @@
+
+
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
+import '../../domain/entities/movie.dart';
+
+class MovieMapper{
+
+static Movie movieDBToEntity (MovieMovieDB movieDB)=>Movie(
+  adult: movieDB.adult,
+  overview: movieDB.overview,
+  popularity: movieDB.popularity,
+  voteCount: movieDB.voteCount,
+  video: movieDB.video,
+  originalLanguage: movieDB.originalLanguage,
+  originalTitle: movieDB.originalTitle,
+  genreIds: movieDB.genreIds.map((e) => e.toString()).toList(),
+  id: movieDB.id,
+  title: movieDB.title,
+  posterPath: (movieDB.posterPath !="")
+  ?"https://image.tmdb.org/t/p/w500${movieDB.posterPath}"
+  :"no poster",
+  backdropPath:( movieDB.backdropPath !="")
+  ?"https://image.tmdb.org/t/p/w500${movieDB.backdropPath}"
+  :"https://th.bing.com/th/id/OIP.Sbq_JGR8ht94tFH1gnUYqgHaHa?w=177&h=180&c=7&r=0&o=5&pid=1.7",
+  releaseDate: movieDB.releaseDate,
+  voteAverage: movieDB.voteAverage
+);
+
+}
