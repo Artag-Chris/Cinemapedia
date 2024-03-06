d5994ccc960e8b0b78e93e42c3195ed433d91f08
Artag/Chris <scristxyz@gmail.com>
2024-03-06 17:56:33 -0500

    rating

--- lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -1,4 +1,5 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/config/helpers/human_format.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
@@ -22,21 +23,19 @@ class MovieHorizontalListView extends StatelessWidget {
       child: Column(children: [
         if (title != null || subtitle != null)
           _Title(title: title, subtitle: subtitle),
-
-          Expanded(child: ListView.builder(
-            itemCount: movie.length,
-            scrollDirection: Axis.horizontal,
-            physics: const BouncingScrollPhysics(),
-            itemBuilder: (context, index) {
-              return _Slide(movie: movie[index]);
-            },
-
-          ))
+        Expanded(
+            child: ListView.builder(
+          itemCount: movie.length,
+          scrollDirection: Axis.horizontal,
+          physics: const BouncingScrollPhysics(),
+          itemBuilder: (context, index) {
+            return _Slide(movie: movie[index]);
+          },
+        ))
       ]),
     );
   }
 }
-
 
 class _Slide extends StatelessWidget {
   final Movie movie;
@@ -45,54 +44,57 @@ class _Slide extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final colors = Theme.of(context).colorScheme;
-    final textStyles= Theme.of(context).textTheme;
+    final textStyles = Theme.of(context).textTheme;
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 8),
-     child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-       children: [
-         SizedBox(
-           width: 150,
-           child: ClipRRect(
-             borderRadius: BorderRadius.circular(20),
-             child: Image.network(
-              movie.posterPath,
-              fit: BoxFit.cover,
-              width: 150,
-              loadingBuilder: (context, child, loadingProgress) {
-                if (loadingProgress != null) {
-                  return const CircularProgressIndicator(
-                    strokeWidth: 2,
-                  );
-                }
-                return FadeIn(child:child );
-              },),
-           )
-         ),
-         //* Titulo
-         const SizedBox(height: 5),
-         SizedBox(
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        SizedBox(
+            width: 150,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(
+                movie.posterPath,
+                fit: BoxFit.cover,
+                width: 150,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress != null) {
+                    return const CircularProgressIndicator(
+                      strokeWidth: 2,
+                    );
+                  }
+                  return FadeIn(child: child);
+                },
+              ),
+            )),
+        //* Titulo
+        const SizedBox(height: 5),
+        SizedBox(
           width: 150,
-          child: Text(movie.title, maxLines: 2, overflow: TextOverflow.ellipsis, style: textStyles.titleSmall),
-         ),
+          child: Text(movie.title,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: textStyles.titleSmall),
+        ),
 
-         //* rating
-         Row(
-           children: [
-             Icon(Icons.star_half_outlined, color: Colors.yellow.shade800),
-             const SizedBox(width: 5),
-             Text(movie.voteAverage.toString(), style: textStyles.bodyMedium?.copyWith(color: Colors.yellow.shade800)),
-             const SizedBox(width: 10),
-             Text("${movie.popularity}", style: textStyles.bodySmall),
-           ]
-         )
-       ]
-     ),
+        //* rating
+        SizedBox(
+          width: 150,
+          child: Row(children: [
+            Icon(Icons.star_half_outlined, color: Colors.yellow.shade800),
+            const SizedBox(width: 5),
+            Text(movie.voteAverage.toString(),
+                style: textStyles.bodyMedium
+                    ?.copyWith(color: Colors.yellow.shade800)),
+            const SizedBox(width: 10),
+            const Spacer(),
+            Text(HumanFormat.number(movie.popularity),
+                style: textStyles.bodyMedium?.copyWith(color: Colors.black87))
+          ]),
+        )
+      ]),
     );
   }
 }
-
-
 
 class _Title extends StatelessWidget {
   final String? title;
