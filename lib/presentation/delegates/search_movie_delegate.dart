a65e2cd67e15c4a07654a5e1c106a53a0660943a
Artag/Chris <scristxyz@gmail.com>
2024-03-13 16:23:27 -0500

    creacion del delegate

---
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -0,0 +1,39 @@
+
+
+import 'package:flutter/material.dart';
+
+class SearchMovieDelegate extends SearchDelegate {
+
+
+@override
+String get searchFieldLabel => "Buscar Película";
+
+
+
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+  return [
+    const Text("Build Actions"),
+  ];
+
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+return const Text("Build Leading");
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+  return   const Text("Build Results");
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+ return     const Text("Build suggestions");
+  }
+
+
+
+}
