b34cc818ca29482a9ce2073f5dbc27518233d612
Artag/Chris <scristxyz@gmail.com>
2024-03-19 21:37:19 -0500

    series datasouerce creado

---
+++ lib/domain/datasources/series.datasource.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SeriesDatasouerce extends StatelessWidget {
+  const SeriesDatasouerce({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
